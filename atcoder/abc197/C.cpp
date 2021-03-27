3fb23de872dca55405db5eae74c2b5f754779b13
Shahil <shahilsingh5501@gmail.com>
2021-03-27 18:29:00 +0530

    Add solution for problem `C - ORXOR` Link: https://atcoder.jp/contests/abc197/tasks/abc197_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc197/submissions/21314118

--- atcoder/abc197/C.cpp
+++ atcoder/abc197/C.cpp
@@ -1,5 +1,5 @@
 // Author: shahil_005
-// Time: 2021-05-14 00:08:20
+// Time: 2021-03-27 17:30:21
 // Problem: C - ORXOR
 // Contest: AtCoder - AtCoder Beginner Contest 197（Sponsored by Panasonic）
 // URL: https://atcoder.jp/contests/abc197/tasks/abc197_c
@@ -8,12 +8,8 @@
 // 
 // Powered by CP Editor (https://cpeditor.org)
 
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
 #include <bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
-// #pragma GCC optimize ("trapv")
 //g++ -static -DONLINE_JUDGE -Wl,--stack=268435456 -O2 -std=c++17
 //g++ -std=c++17 -Wshadow -Wall -o "%e" "%f" -O2 -Wno-unused-result
 //g++ -std=c++17 -Wshadow -Wall -o "%e" "%f" -g  -D_GLIBCXX_DEBUG
@@ -29,9 +25,6 @@ using namespace __gnu_pbds;
 #define ss                      second
 #define fix(f,n) 		std::fixed<<std::setprecision(n)<<f
 #define pll 			pair<ll,ll>
-#define pii 			pair<int,int>
-#define MOD      		1000000007
-#define MOD1   			998244353
 //Time:
 #define debug2(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
 #define time__(d) \
@@ -54,7 +47,7 @@ void __print(char x) {cout << '\'' << x << '\'';}
 void __print(const char *x) {cout << '\"' << x << '\"';}
 void __print(const string &x) {cout << '\"' << x << '\"';}
 void __print(bool x) {cout << (x ? "true" : "false");}
- 
+
 template<typename T, typename V>
 void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
 template<typename T>
@@ -89,75 +82,80 @@ void arr2_(T1 *a,T2 x1,T3 x2,T4 y1,T5 y2){
 // https://codeforces.com/blog/entry/58786
 // mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-template <typename T1,typename T2> auto rng2(T1 a,T2 b){return a + rng() % (b - a + 1);}
+template <typename T>
+T rng2(T a,T b){
+	return a + rng() % (b - a + 1);
+}
 //Functions:
 void start(){
 // #ifndef ONLINE_JUDGE
 	// freopen("input.txt","r",stdin);
 	// freopen("output.txt","w",stdout);
 // #endif
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);cout.tie(NULL);	// Remove in interactive problems
-	// srand(time(NULL));	
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	srand(time(NULL));
 }
- 
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;	//https://codeforces.com/blog/entry/11080
-//find_by_order(k) : returns an iterator to the k-th largest element (counting from zero)
-//order_of_key(k) : number of elements in set that are strictly smaller than k
-template<typename u,typename v> auto min(u a,v b){return a>b?b:a;}
-template<typename u,typename v> auto max(u a,v b){return a<b?b:a;}
 template<typename T> T power(T x,T y){if(y == 0){return 1;} T p = power(x, y/2); p = (p * p); return (y%2 == 0)? p : (x * p);}  
 template<typename T> T powm(T x,T y,T m){T ans=1,r=1;x%=m;while(r>0&&r<=y){if(r&y){ans*=x;ans%=m;} r<<=1;x*=x;x%=m;}return ans;}   
 template<typename T> T gcd(T a,T b) { if (b == 0){return a;} return gcd(b, a % b);}  
 template<typename T> T lcm(T a,T b){T lar = max(a, b);T small = min(a, b);for (T i = lar; ; i += lar) { if (i % small == 0){ return i;} } }
-template<typename T> T modinv(T a) { return powm(a, MOD-2); } //for prime
 template<typename T> T mod(T a,T b){return ((a%b)+b)%b;}
-template<typename T> T modsum(T a, T b, T m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-template<typename T> T modmul(T a, T b, T m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-template<typename T> T modmulf(T a,T b,T mod) {T res=0;a%= mod;while(b){if(b&1)res=(res+a)%mod;a=(2*a)%mod;b>>=1;}return res;}
-template<typename T> T mod_sub(T a, T b, T m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-template<typename T> T mod_div(T a, T b, T m) {a = a % m; b = b % m; return (modmul(a, modinv(b, m), m) + m) % m;}  //only for prime m
-template<typename T> T fact(T r){ T ans=1;while(r)ans=modmul(ans,r),r--;return ans; }
-template<typename T> T invfact(T r){T ans=1;while(r)ans=modmul(ans,modinv(r)),r--; return ans;}
-template<typename T> bool cmp1(const pair<T,T> &a, const pair<T,T> &b) {  return (a.ss<b.ss); }//sortbysec
-template<typename T> bool cmp2(const pair<T,T> &a, const pair<T,T> &b) {  return (a.ff>b.ff); }//sortbydec
+template<typename T> T mul(T a, T b,T mod){  return ((a * b)+mod)%mod;}
+template<typename T> T modmul(T a,T b,T mod) {T res=0;a%= mod;while(b){if(b&1)res=(res+a)%mod;a=(2*a)%mod;b>>=1;}return res;}
+template<typename T> T modsum(T a, T b,T mod){  return (a + b) % mod;} 
+bool cmp1(const pair<ll,ll> &a, const pair<ll,ll> &b) {  return (a.ss<b.ss); }//sortbysec
+bool cmp2(const pair<ll,ll> &a, const pair<ll,ll> &b) {  return (a.ff>b.ff); }//sortbydec
 // void line(ll x){while(x--){cout<<"-";}cout<<endl;}
 bool overflow(long double a, long double b){return a * b > 1e18 + 10;}
-const long long N=(long long)(1e5+5);
-const long long inf=(long long)(1e18);
+
+const long long N=(long long)(1e5+1);
+const long long MOD=(long long)(1e9+7);
+const long long inf=(long long)((1<<30)+1);
+ll n;
+ll a[21];
+ll ans=inf;
+void recur(ll x,vector<ll> v,vector<ll> v2){
+	if(x==n+1){
+		ll y=0;
+		for(auto it:v){
+			y=y^it;
+		}
+		// debug(v2);
+		// debug(v,y);
+		
+		ans=min(ans,y);
+		return;
+	}
+	ll val=0;
+	v2.pb(x);
+	for(int i=x;i<=n;i++){
+		val=val|a[i];
+		v.pb(val);
+		recur(i+1,v,v2);
+		v.pop_back();
+	}
+}
 void solve(int tc)
 {
-	ll n;
+	// cout<<inf<<endl;
 	cin>>n;
-	vector<ll> a(n);
-	for(int i=1;i<=n;i++) cin>>a[i-1];
-	ll ans=1e18;
-	for(ll mask=0;mask<(1<<n);mask++){
-		ll cur=0,tot=0;
-		for(ll i=0;i<n;i++){
-			if((mask>>i)&1){
-				cur|=a[i];
-				tot^=cur;
-				cur=0;
-			}
-			else{
-				cur|=a[i];
-			}
-		}
-		tot^=cur;
-		ans=min(ans,tot);
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
 	}
+	vector<ll> v;
+	vector<ll> v2;
+	recur(1,v,v2);
 	cout<<ans;
 }
 int main(){
 	start();
-	time__("solve"){
-		int t=1;
-		// cin>>t;
-		for(int tc=1;tc<=t;tc++){	
+	int t=1;
+	// cin>>t;
+	for(int tc=1;tc<=t;tc++){
+		time__("solve"){
 			// google(tc);
 			solve(tc); 
-		}
+                }
 	}
         return 0;
 }
